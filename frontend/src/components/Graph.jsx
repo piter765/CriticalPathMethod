4c46faa6a0983a9843543027ef8843dec1c04f01
Piotr <piotrektymula1@gmail.com>
2024-03-08 20:14:59 +0100

    add graphs for cpm

---
+++ frontend/src/components/Graph.jsx
@@ -0,0 +1,43 @@
+import { Graph } from "react-d3-graph";
+
+// graph payload (with minimalist structure)
+const data = {
+  nodes: [{ id: "Harry" }, { id: "Sally" }, { id: "Alice" }],
+  links: [
+    { source: "Harry", target: "Sally" },
+    { source: "Harry", target: "Alice" },
+  ],
+};
+
+// the graph configuration, just override the ones you need
+const myConfig = {
+  nodeHighlightBehavior: true,
+  node: {
+    color: "lightgreen",
+    size: 120,
+    highlightStrokeColor: "blue",
+  },
+  link: {
+    highlightColor: "lightblue",
+  },
+};
+
+const onClickNode = function (nodeId) {
+  window.alert(`Clicked node ${nodeId}`);
+};
+
+const onClickLink = function (source, target) {
+  window.alert(`Clicked link between ${source} and ${target}`);
+};
+
+export default function CpmGraph() {
+  return (
+    <Graph
+      id="graph-id" // id is mandatory
+      data={data}
+      config={myConfig}
+      onClickNode={onClickNode}
+      onClickLink={onClickLink}
+    />
+  );
+}
